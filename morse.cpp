5a6b275be21f0309401513d80b18fa976a1d8bb3
Jon <jjones646@gmail.com>
2016-02-14 03:41:40 -0500

    adding file that will be parsed and setting program to echo the input file

--- morse.cpp
+++ morse.cpp
@@ -1,13 +1,14 @@
 /**
  * morse.cpp
- * 
+ *
  * Jonathan Jones
- * A simple program to parse in a file and convert to morse code dots & dashes. 
+ * A simple program to parse in a file and convert to morse code dots & dashes.
  */
 
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
+#include <string>
 
 #include "morse.hpp"
 
@@ -16,25 +17,35 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
     // make sure we are given a file to parse
-    if(argc != 2) {
-        cout << "usage: ./morse <file>" << endl;
+    if ( argc != 2 ) {
+        cout << "usage: " << argv[0] << " <file>" << endl;
         return EXIT_FAILURE;
     }
 
     // check if the given file exists
     string fn(argv[1]);
 
-    if( fileExists(fn) == false ) {
+    if ( fileExists(fn) == false ) {
         cout << fn << " does not exist" << endl;
         return EXIT_FAILURE;
     }
 
-    // if we make it here, the file exists - let's read it in
-    ofstream infile;
+    // if we make it here, the file exists
+    // let's open the file now
+    ifstream infile;
     infile.open(fn.c_str());
 
+    // let's read in the file now
+    string line;
+    while ( !infile.eof() ) {
+        getline(infile, line);
+        cout << line;
+    }
 
+    // cout << "error opening " << fn << endl;
+    // return EXIT_FAILURE;
 
+    // close the file & terminate the program
     infile.close();
     exit(EXIT_SUCCESS);
 }
