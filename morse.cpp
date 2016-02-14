b653e93e891bdce14e30e310c7f46499ce811a06
Jon <jjones646@gmail.com>
2016-02-14 03:31:48 -0500

    adding file checking capabilities

--- morse.cpp
+++ morse.cpp
@@ -1,16 +1,18 @@
 /**
- *  Jonathan Jones
- *  
- *  A simple program to parse in a file and convert to morse code dots & dashes.
- *  
- *  morse.cpp
+ * morse.cpp
+ * 
+ * Jonathan Jones
+ * A simple program to parse in a file and convert to morse code dots & dashes. 
  */
 
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
 
+#include "morse.hpp"
+
 using namespace std;
+
 
 int main(int argc, char* argv[]) {
     // make sure we are given a file to parse
@@ -19,8 +21,20 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
-    ofstream infile;
-    infile.open(argv[1]);
+    // check if the given file exists
+    string fn(argv[1]);
 
+    if( fileExists(fn) == false ) {
+        cout << fn << " does not exist" << endl;
+        return EXIT_FAILURE;
+    }
+
+    // if we make it here, the file exists - let's read it in
+    ofstream infile;
+    infile.open(fn.c_str());
+
+
+
+    infile.close();
     exit(EXIT_SUCCESS);
 }
---
+++ morse.hpp
@@ -0,0 +1,23 @@
+
+#include <string>
+
+#include <sys/stat.h>
+
+
+// Function: fileExists
+/**
+    Check if a file exists
+@param[in] filename - the name of the file to check
+
+@return    true if the file exists, else false
+
+*/
+bool fileExists(const std::string& filename)
+{
+    struct stat buf;
+
+    if (stat(filename.c_str(), &buf) != -1)
+        return true;
+
+    return false;
+}
